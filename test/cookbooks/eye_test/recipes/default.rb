3499a49b97bfd0fde038dc05680ba48abe3c2aa4
Holger Amann <holger@sauspiel.de>
2013-10-03 01:46:53 +0200

    unicorn example

--- test/cookbooks/eye_test/recipes/default.rb
+++ test/cookbooks/eye_test/recipes/default.rb
@@ -6,6 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+gem_package "unicorn"
 
 include_recipe "eye::default"
 
@@ -27,4 +28,24 @@ eye_app "sleep_root" do
   pid_file "/var/tmp/sleep_root.pid"
   init_script_prefix ''
   daemonize true
+end
+
+
+
+rails_dir = '/var/www/www.railsapp.dev/current'
+directory rails_dir do
+  user 'vagrant'
+  group 'users'
+  recursive true
+end
+
+eye_app "test_unicorn" do
+  user_srv true
+  user_srv_uid "vagrant"
+  user_srv_gid "users"
+  template "test_unicorn.conf.erb"
+  cookbook "eye_test"
+  variables :ruby => "#{node['languages']['ruby']['bin_dir']}/ruby",
+    :environment => 'test',
+    :working_dir => rails_dir
 end
