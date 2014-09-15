fb156b9ed3cf4a85061a3352d97b6543a89ffb8b
Holger Amann <holger@sauspiel.de>
2014-09-15 11:22:08 +0200

    new version for supermarket due to missing resources folder

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'holger@fehu.org'
 license          'Apache 2.0'
 description      'Installs eye gem and configures it to manage services, includes eye_service LWRP'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.7.3'
+version          '0.7.4'
 recipe "eye::default", "Installs eye rubygem"
